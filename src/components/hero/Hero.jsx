851d8c25294aa263662f4ae0f3f40a83f43164ad
isinnur <isinngnyy@gmail.com>
2024-05-02 18:09:56 +0300

    feat: change background

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,10 @@
 import styles from './hero.module.css';
 const Hero = () => {
-  return <div className={styles.hero}></div>;
+  return (
+    <div className={styles.hero}>
+      <h1>Hackathon</h1>
+    </div>
+  );
 };
 
 export default Hero;
