6fa9778ab49f51f07091b476aaaa4b935571ef43
Semanur Özkan <semanurozkan4444@gmail.com>
2024-05-02 23:26:16 +0300

    feat(navbar): create navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,38 @@ import React from 'react';
 import styles from './navbar.module.css';
 
 const Navbar = () => {
-  return <div className={styles.navbar}>Navbar</div>;
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.logo}>
+        <h3>HACK HAFTASI</h3>
+      </div>
+
+      <div>
+        <ul className={styles.navCenter}>
+          <li className={styles.li}>
+            <a href='www'>Bilgilendirme</a>
+          </li>
+          <li className={styles.li}>
+            <a href='www'>Ödüller</a>
+          </li>
+          <li className={styles.li}>
+            <a href='www'>Jüri</a>
+          </li>
+          <li className={styles.li}>
+            <a href='www'>Sponsorlar</a>
+          </li>
+          <li className={styles.li}>
+            <a href='www'>Tiny</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.navEnd}>
+        <button className={styles.button}>BAŞVUR</button>
+        <button className={styles.button}>SPONSOR OL</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
