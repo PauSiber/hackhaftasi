0218579512098943955f01d12d788d53e3e50709
Enes Kaya <eneskaya5261@gmail.com>
2024-05-12 16:00:59 +0300

    fix: update sponsor's logos

--- src/components/sponsor/Sponsor.jsx
+++ src/components/sponsor/Sponsor.jsx
@@ -2,74 +2,74 @@ import React from 'react';
 import styles from './sponsor.module.css';
 
 const Sponsor = () => {
-  return (
-    <div id='sponsor' className={styles.container}>
-      <div className={styles.title}>
-        <span>SPONSORLAR</span>
-      </div>
-      <div className={styles.sponsors}>
-        <div className={styles.img}>
-          <img
-            onClick={function redirectToLinkedIn() {
-              window.open('https://pausiber.xyz/', '_blank');
-            }}
-            src='/images/pausiber-logo-cekirdek.png'
-            alt=''
-          />
-        </div>
+    return (
+        <div id='sponsor' className={styles.container}>
+            <div className={styles.title}>
+                <span>SPONSORLAR</span>
+            </div>
+            <div className={styles.sponsors}>
+                <div className={styles.img}>
+                    <img
+                        onClick={function redirectToLinkedIn() {
+                            window.open('https://pausiber.xyz/', '_blank');
+                        }}
+                        src='/images/pausiber-logo-cekirdek.png'
+                        alt=''
+                    />
+                </div>
 
-        <div className={styles.img}>
-          <img
-            onClick={function redirectToLinkedIn() {
-              window.open('https://siberkulupler.com/', '_blank');
-            }}
-            src='/images/skb_logo.png'
-            alt=''
-          />
-        </div>
+                <div className={styles.img}>
+                    <img
+                        onClick={function redirectToLinkedIn() {
+                            window.open('https://siberkulupler.com/', '_blank');
+                        }}
+                        src='/images/skb_logo.png'
+                        alt=''
+                    />
+                </div>
 
-        <div className={styles.img}>
-          <img
-            onClick={function redirectToLinkedIn() {
-              window.open('https://ratel.com.tr/tr/', '_blank');
-            }}
-            src='/images/ratel_logo.png'
-            alt=''
-          />
-        </div>
+                <div className={styles.img}>
+                    <img
+                        onClick={function redirectToLinkedIn() {
+                            window.open('https://ratel.com.tr/tr/', '_blank');
+                        }}
+                        src='/images/ratel_logo.webp'
+                        alt=''
+                    />
+                </div>
 
-        <div className={styles.img}>
-          <img
-            onClick={function redirectToLinkedIn() {
-              window.open('https://www.tubitak.gov.tr/', '_blank');
-            }}
-            src='/images/tubitak_logo.png'
-            alt=''
-          />
-        </div>
+                <div className={styles.img}>
+                    <img
+                        onClick={function redirectToLinkedIn() {
+                            window.open('https://www.tubitak.gov.tr/', '_blank');
+                        }}
+                        src='/images/tubitak_logo.png'
+                        alt=''
+                    />
+                </div>
 
-        <div className={styles.img}>
-          <img
-            onClick={function redirectToLinkedIn() {
-              window.open('https://secureway.com.tr/', '_blank');
-            }}
-            src='/images/secureWay.jpeg'
-            alt=''
-          />
-        </div>
+                <div className={styles.img}>
+                    <img
+                        onClick={function redirectToLinkedIn() {
+                            window.open('https://secureway.com.tr/', '_blank');
+                        }}
+                        src='/images/secureWay.jpeg'
+                        alt=''
+                    />
+                </div>
 
-        <div className={styles.img}>
-          <img
-            onClick={function redirectToLinkedIn() {
-              window.open('https://www.netinternet.com.tr/', '_blank');
-            }}
-            src='/images/netinternet.svg'
-            alt=''
-          />
+                <div className={styles.img}>
+                    <img
+                        onClick={function redirectToLinkedIn() {
+                            window.open('https://www.netinternet.com.tr/', '_blank');
+                        }}
+                        src='/images/netinternet.svg'
+                        alt=''
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Sponsor;
